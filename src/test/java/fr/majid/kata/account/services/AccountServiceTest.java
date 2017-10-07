54d6f046c9affc74db9fff38483d0b96797a6cb1
majid.mortada <majid.mortada@gmail.com>
2017-10-08 00:55:58 +0200

    [US2] Withdraw opeartion finished test.

--- src/test/java/fr/majid/kata/account/services/AccountServiceTest.java
+++ src/test/java/fr/majid/kata/account/services/AccountServiceTest.java
@@ -3,6 +3,8 @@ package fr.majid.kata.account.services;
 import static fr.majid.kata.builder.GenericBuilder.of;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import javax.security.auth.login.AccountNotFoundException;
 
@@ -10,6 +12,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.runners.MockitoJUnitRunner;
 
@@ -18,7 +21,6 @@ import fr.majid.kata.model.Amount;
 import fr.majid.kata.model.Customer;
 import fr.majid.kata.repository.AccountRepository;
 import fr.majid.kata.services.AccountService;
-
 /**
  *
  * @author mortada majid
@@ -64,16 +66,27 @@ public class AccountServiceTest {
 
 	@Test
 	public void should_withdraw_from_account() throws Exception {
+		String accountNumber = "3200666";
 		Customer custom = of(Customer::new)
                 .with(Customer::setId,1L).build();
 		Account account = of(Account::new)
 				.with(Account::setId,1L)
-	            .with(Account::setNumero,"3200666")
+	            .with(Account::setNumero,accountNumber)
 	            .with(Account::setCustomer,custom)
 	            .with(Account::setSolde,1000L)
 	            .build();
-
-	        accountService.withdraw(new Amount(500L), account);
+		Account accountUpdate = of(Account::new)
+				.with(Account::setId,1L)
+	            .with(Account::setNumero,accountNumber)
+	            .with(Account::setCustomer,custom)
+	            .with(Account::setSolde,500L)
+	            .build();
+		doReturn(accountUpdate).when(accountRepository).save(accountUpdate);
+		
+		Account accountWithNewDeposit = accountService.withdraw(new Amount(100L), account);
+		
+		verify(accountRepository, times(1)).save(Mockito.any(Account.class));
+		assertThat(accountWithNewDeposit).isEqualToComparingFieldByField(accountUpdate);
 
 	    }
 
